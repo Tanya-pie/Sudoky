9f8ee2cf2ad897283246f7be162d9f38eb1df9c8
Tanya-pie <tantanlavrik@gmail.com>
2024-12-01 15:45:51 +0300

    Update Cell.cs

--- Sudoku-CSharp-main/src/Sudoku/Models/Cell.cs
+++ Sudoku-CSharp-main/src/Sudoku/Models/Cell.cs
@@ -1,40 +1,32 @@
-﻿namespace Sudoku.Models
+namespace Sudoku.Models
 {
-    /// <summary>
-    /// Cell to hold details of index, value, group number, postion.
-    /// </summary>
+
+    /// Ячейка для хранения сведений об индексе, значении, номере группы, позиции
+
     public class Cell
     {
-        /// <summary>
-        /// The Index Property
-        /// </summary>
-        /// <returns>The Index Number</returns>
+
+        /// Свойство индекса
+
         public int Index { get; private set; }
-        /// <summary>
-        /// The Value Property
-        /// </summary>
-        /// <returns>The Value Number</returns>
+
+        /// Свойство Value (значения)
+
         public int Value { get; set; }
-        /// <summary>
-        /// The Group Number Property
-        /// </summary>
-        /// <returns>The Group Number</returns>
+
+        /// Свойство номера группы (подсетки)
+
+
         public int GroupNumber { get; private set; }
-        /// <summary>
-        /// The Position Property
-        /// </summary>
-        /// <returns>The Postion</returns>
+
+        /// Свойство Position (положение)
+
         public Position Position { get; private set; }
 
 
-        /// <summary>
-        /// Cell Constructor
-        /// </summary>
-        /// <param name="index">The index.</param>
-        /// <param name="value">The value.</param>
-        /// <param name="groupNumber">The Group Number</param>
-        /// <param name="position">The Position</param>
-        /// <returns>Cell</returns>
+
+        /// Конструктор ячеек
+
         public Cell(int index, int value, int groupNumber, Position position)
         {
             Index = index;
